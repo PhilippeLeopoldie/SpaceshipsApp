5a8e4a25ad3203aa7d82cabe25b3737352afe9a7
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-09 14:21:38 +0200

    Add ApplicationContext and refactor ApplicationUser namespace
    - Introduced `ApplicationContext` class inheriting from `IdentityDbContext` for identity management.
    - Updated namespaces in `ApplicationUser.cs` for project restructuring while retaining user properties.

---
+++ Spaceships.Infrastructure/Persistance/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceships.Infrastructure.Persistance;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
+{
+}
--- Spaceships.Infrastructure/Persistance/ApplicationUser.cs
+++ Spaceships.Infrastructure/Persistance/ApplicationUser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Spaceships.Infrastructure.Persistance.Users;
+namespace Spaceships.Infrastructure.Persistance;
 
 public class ApplicationUser : IdentityUser
 {
