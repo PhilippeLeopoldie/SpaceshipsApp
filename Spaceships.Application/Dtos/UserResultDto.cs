df5a8ca0a74c45679e9bcbdc65cdd939d8d4ff24
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-09 16:26:52 +0200

    Add user DTOs and update identity service interface
    Introduced `UserProfileDto` and `UserResultDto` for user data management. Removed folder references for `Dtos` and `Users` in the project file, indicating a potential restructuring. Enhanced `IIdentityUserService` with methods for user creation and sign-in, utilizing the new DTOs.

---
+++ Spaceships.Application/Dtos/UserResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceships.Application.Dtos;
+
+public record UserResultDto(string? ErrorMessage)
+{
+    public bool Succeded => ErrorMessage == null;
+}
