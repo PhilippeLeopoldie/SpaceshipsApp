9c711808cd18180c661b43638bbb60481b1b76df
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-09 16:53:09 +0200

    Add IdentityUserService for user management functionality
    Introduces the `IdentityUserService` class to handle user creation and sign-in using ASP.NET Core Identity.
    Implements `CreateUserAsync` to create users and `SignInAsync` for user authentication, returning relevant error messages.
    Includes necessary using directives for the new functionality.
    
    Add IdentityUserService for user management functionality
    
    This commit introduces a new `IdentityUserService` class that implements user management features using ASP.NET Core Identity. It includes methods for creating and signing in users, leveraging `UserManager` and `SignInManager`.
    
    Additionally, the project file `Spaceships.Infrastructure.csproj` has been updated to remove the folder reference for "Persistance\Services\", reflecting a potential reorganization of the project structure while maintaining necessary package references for user management and database interactions.

---
+++ Spaceships.Infrastructure/Services/IdentityUserService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Spaceships.Application.Dtos;
+using Spaceships.Application.Users;
+using Spaceships.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Spaceships.Infrastructure.Services;
+
+public class IdentityUserService(
+    UserManager<ApplicationUser> useManager,
+    SignInManager<ApplicationUser> signInManager) : IIdentityUserService
+{
+    public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
+    {
+        var result = await useManager.CreateAsync(new ApplicationUser
+        {
+            UserName = user.Email,
+            Email = user.Email, 
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+        }, password);
+        return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
+    }
+
+    public async Task<UserResultDto> SignInAsync(string email, string password)
+    {
+        var result = await signInManager.PasswordSignInAsync(email, password, false, false);
+        return new UserResultDto(result.Succeeded ? null : "Invalid User credentials");
+    }
+}
