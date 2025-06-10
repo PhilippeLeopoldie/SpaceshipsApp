9c4d852a022bcd60a9c1cfabaeb5b01ead65dddd
Nidora <ludwigjohansson@gmail.com>
2025-06-10 11:17:49 +0200

    Add SpaceShip DbSet to ApplicationContext
    This commit introduces a new using directive for
    `Spaceships.Domain.Entities` in `ApplicationContext.cs`
    and adds a `DbSet<SpaceShip>?` property to manage
    `SpaceShip` entities within the database context,
    enabling CRUD operations for these records.

--- Spaceships.Infrastructure/Persistance/ApplicationContext.cs
+++ Spaceships.Infrastructure/Persistance/ApplicationContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Spaceships.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Spaceships.Infrastructure.Persistance;
 public class ApplicationContext(DbContextOptions<ApplicationContext> options)
     : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
 {
+    public DbSet<SpaceShip>? SpaceShip { get; set; }
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         var modifiedEntries = ChangeTracker.Entries()
