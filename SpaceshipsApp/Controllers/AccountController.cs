af59e6d4534d14e4645056b564634864b5668a83
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-10 10:00:24 +0200

    Add AccountController with Members and Register actions
    Updated AccountController to include authorization and MVC
    functionality, adding Members and Register action methods.
    Introduced new package reference in the project file and
    removed the Views\Account folder reference. Created or
    updated Members.cshtml to display the Members page.

---
+++ SpaceshipsApp/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Spaceships.Application.Users;
+
+namespace Spaceships.Web.Controllers;
+
+public class AccountController(IUserService user) : Controller
+{
+    [Authorize]
+    [HttpGet("")]
+    [HttpGet("members")]
+    public IActionResult Members()
+    {
+        return View();
+    }
+
+    [HttpGet("register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
