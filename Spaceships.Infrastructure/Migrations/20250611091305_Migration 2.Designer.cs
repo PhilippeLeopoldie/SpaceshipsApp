4b3f253ba96c2066cfcada1553100fd0b4c14c77
Keshti Gyllinger <gyllinger@gmail.com>
2025-06-11 11:24:21 +0200

    Update spaceship images and enhance authentication links
    - Replaced external image URLs with local paths for seven spaceship entries in `ApplicationContextModelSnapshot.cs`.
    - Updated `_Layout.cshtml` to conditionally display the "Members" link and "Logout" option based on user authentication status.
    - Added migration file `20250611091305_Migration 2.cs` to update the database with new local image paths.
    - Updated designer file `20250611091305_Migration 2.Designer.cs` to reflect changes in the migration.

--- Spaceships.Infrastructure/Migrations/20250611091305_Migration 2.Designer.cs
+++ Spaceships.Infrastructure/Migrations/20250611091305_Migration 2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spaceships.Infrastructure.Persistance;
 
@@ -11,9 +12,11 @@ using Spaceships.Infrastructure.Persistance;
 namespace Spaceships.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250611091305_Migration 2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
