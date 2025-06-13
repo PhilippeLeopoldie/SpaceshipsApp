78513799c41c8fcc2087cebe659dba9e3d1566d5
Keshti Gyllinger <gyllinger@gmail.com>
2025-06-13 09:49:44 +0200

    Refactor registration form and update scripts
    Restructure form elements in `Register.cshtml` for improved organization and readability. Replace inline JavaScript with external jQuery and validation scripts. Move checkbox handling logic to `changeBodyClass.js` to update the body class instead of the form class, enhancing maintainability and clarity.

---
+++ SpaceshipsApp/wwwroot/Scripts/changeBodyClass.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.getElementById('isAdminCheckbox');
+    // var form = document.getElementById('registerForm');
+    function updateBodyClass() {
+        if (checkbox.checked) {
+            document.body.classList.add('IsAdmin');
+        } else {
+            document.body.classList.remove('IsAdmin');
+        }
+    }
+    checkbox.addEventListener('change', updateBodyClass);
+    updateBodyClass(); // Set initial state
+});
